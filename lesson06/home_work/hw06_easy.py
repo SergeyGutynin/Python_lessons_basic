4e19289ef0221b58be8e694e35f69b5ec4c24fe4
Sergey Gutynin <gutynin.sv@gmail.com>
2018-11-06 11:59:14 +0400

    Complited easy and normal tasks

--- lesson06/home_work/hw06_easy.py
+++ lesson06/home_work/hw06_easy.py
@@ -1,9 +1,114 @@
 # Задача-1: Написать класс для фигуры-треугольника, заданного координатами трех точек.
 # Определить методы, позволяющие вычислить: площадь, высоту и периметр фигуры.
 
+class Triangle:
+
+    def __init__(self, x1, y1, x2, y2, x3, y3):
+        self.points = {'a': [x1, y1],
+                       'b': [x2, y2],
+                       'c': [x3, y3]
+                       }
+        self.sides = [self.side_len(self.points['a'], self.points['b']),
+                      self.side_len(self.points['b'], self.points['c']),
+                      self.side_len(self.points['c'], self.points['a'])]
+
+    @staticmethod
+    def side_len(pnt1, pnt2):
+        x = 0
+        y = 1
+        return ((pnt2[x]-pnt1[x])**2+(pnt2[y]-pnt1[y])**2)**0.5
+
+    def perimeter(self):
+        return sum(self.sides)
+
+    def height(self):
+        base_len = max(self.sides)
+        base_side = self.sides.index(base_len)
+        other_sides = self.sides.copy()
+        other_sides.pop(base_side)
+        if sum(other_sides)>base_side:
+            p = self.perimeter()/2
+            h = (2/base_len)*((p * (p - other_sides[0]) * (p - other_sides[1]) * (p - base_len))**0.5)
+            return h
+
+    def square(self):
+
+        h = self.height()
+        if h:
+            s = 0.5 * h * max(self.sides)
+            return s
+
+
+triangle1 = Triangle(0,0,0,1,1,0)
+
+print(f'Длины сторон треугольника {triangle1.sides}')
+print(f'Периметр треугольника равен {triangle1.perimeter()}')
+print(f'Высота треугольника к основанию равна {triangle1.height()}')
+print(f'Площадь треугольника равна {triangle1.square()}')
+
 
 # Задача-2: Написать Класс "Равнобочная трапеция", заданной координатами 4-х точек.
 # Предусмотреть в классе методы:
 # проверка, является ли фигура равнобочной трапецией;
 # вычисления: длины сторон, периметр, площадь.
 
+class EquilTrapezoid:
+
+    def __init__(self, x1, y1, x2, y2, x3, y3, x4, y4):
+        self.points = {'a': [x1, y1],
+                       'b': [x2, y2],
+                       'c': [x3, y3],
+                       'd': [x4, y4],
+                       }
+        self.sides = self.sides_len()
+
+    @staticmethod
+    def side_len(pnt1, pnt2):
+        x = 0
+        y = 1
+        return ((pnt2[x] - pnt1[x]) ** 2 + (pnt2[y] - pnt1[y]) ** 2) ** 0.5
+
+    def sides_len(self):
+        sides = [self.side_len(self.points['a'], self.points['b']),
+                 self.side_len(self.points['b'], self.points['c']),
+                 self.side_len(self.points['c'], self.points['d']),
+                 self.side_len(self.points['d'], self.points['a'])
+                 ]
+
+        print(sides)
+
+        x = 0
+        y = 1
+
+        if (sides[0] == sides[2] and
+            self.points['a'][y] == self.points['d'][y] and
+            self.points['c'][y] == self.points['b'][y]) or \
+            (sides[1] == sides[3] and
+             self.points['a'][x] == self.points['b'][x] and
+             self.points['c'][x] == self.points['d'][x]):
+            return sides
+        else:
+            print('Фигура не является равнобокой трапецией')
+
+    def perimeter(self):
+        if self.sides:
+            return sum(self.sides)
+
+    def square(self):
+
+        if self.sides:
+
+            x = 0
+            y = 1
+
+            if self.sides[0] == self.sides[2]:
+                return (self.sides[1]+self.sides[3])/2 * (self.points['b'][y] - self.points['a'][y])
+
+            elif self.sides[1] == self.sides[3]:
+                return (self.sides[0] + self.sides[2]) / 2 * (self.points['d'][x] - self.points['a'][x])
+
+equal_trapezoid1 = EquilTrapezoid(0,0,1,1,2,1,3,0)
+
+print(f'Длины сторон трапеции {equal_trapezoid1.sides}')
+print(f'Периметр трапеции равен {equal_trapezoid1.perimeter()}')
+print(f'Площадь трапции равна {equal_trapezoid1.square()}')
--- lesson06/home_work/hw06_normal.py
+++ lesson06/home_work/hw06_normal.py
@@ -15,3 +15,147 @@
 #  (Ученик --> Класс --> Учителя --> Предметы)
 # 4. Узнать ФИО родителей указанного ученика
 # 5. Получить список всех Учителей, преподающих в указанном классе
+
+
+class School:
+
+    def __init__(self, name, classes=None, teachers=None):
+        self.name = name
+
+        self._classes = classes
+        self._teachers = teachers
+
+    def get_classes(self):
+        return self._classes.copy()
+
+    def get_class_students(self, class_name):
+        for el in self._classes:
+            if el.name == class_name:
+                return el.students
+
+    def print_class_students(self, class_name):
+        result = self.get_class_students(class_name)
+        print(f'Class name {class_name}')
+        for ind, st in enumerate(result):
+            print(f'{ind+1}. {st.name}')
+
+    def get_student_subjects(self, student_name):
+        for cl in self._classes:
+            for st in cl.students:
+                if st.name == student_name:
+                    tc_names = [t.name for t in self._teachers if t.subjects in cl.subjects]
+                    return student_name, cl.name, tc_names, cl.subjects
+
+    def print_student_subjects(self, student_name):
+        result = self.get_student_subjects(student_name)
+        print(f'Student {result[0]} in class {result[1]} has teachers {result[2]} in subjects {result[3]}')
+
+    def get_student_parents(self, student_name):
+        for cl in self._classes:
+            for st in cl.students:
+                if st.name == student_name:
+                    return st.father, st.mother
+
+    def print_student_parents(self, student_name):
+        result = self.get_student_parents(student_name)
+        print(f'Student {student_name} has parents {result}')
+
+    def get_class_teachers(self, class_name):
+        for cl in self._classes:
+            if cl.name == class_name:
+                tc_names = [t.name for t in self._teachers if t.subjects in cl.subjects]
+                return tc_names
+
+    def print_class_teachers(self, class_name):
+        result = self.get_class_teachers(class_name)
+        print(f'Class {class_name} teachers {result}')
+
+
+class Class_:
+
+    def __init__(self, name, students=None, subjects=None):
+        self.name = name
+        self.students = students
+        self.subjects = subjects
+
+    def __repr__(self):
+        return f"{self.name}"
+
+
+class Person:
+
+    def __init__(self, lastname, firstname, middlename):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.name = f'{lastname} {firstname[0]}.{middlename[0]}.'
+
+    def __repr__(self):
+        return f"{self.firstname} {self.middlename} {self.lastname}"
+
+
+class Teacher(Person):
+    def __init__(self, lastname, firstname, middlename, subjects):
+        super().__init__(lastname, firstname, middlename)
+        self.subjects = subjects
+
+    def __repr__(self):
+        return f"{self.firstname} {self.middlename} {self.lastname} ({self.subjects})"
+
+
+class Student(Person):
+    def __init__(self, lastname, firstname, middlename, father=None, mother=None):
+        super().__init__(lastname, firstname, middlename)
+        self.father = father
+        self.mother = mother
+
+    def __repr__(self):
+        return f"{self.firstname} {self.middlename} {self.lastname} ({self.father}, {self.mother})"
+
+
+# Generate parents and students
+parents = []
+students = []
+
+start = 65
+number = 12
+
+for ind in range(start, start+number, 3):
+    pr1 = Person(chr(ind),chr(ind),chr(ind))
+    pr2 = Person(chr(ind+1), chr(ind+1), chr(ind+1))
+    students.append(Student(chr(ind+2), chr(ind+2), chr(ind+2), pr1, pr2))
+    parents.extend([pr1, pr2])
+
+print(f'Parents {parents}')
+print(f'Students {students}')
+
+# Generate teachers
+
+subjects = ['math', 'phy', 'bio']
+
+teachers = []
+
+start += number
+number = len(subjects)
+
+for ind in range(start, start+number):
+    teachers.append(Teacher(chr(ind), chr(ind), chr(ind), subjects[ind-start]))
+
+print(f'Teachers {teachers}')
+
+# Generate classes
+stcl1 = students[int(len(students)/2):]
+sbjcl1 = ['math', 'phy']
+
+stcl2 = students[:int(len(students)/2)]
+sbjcl2 = ['math', 'bio']
+
+classes = [Class_('1A', stcl1, sbjcl1), Class_('1B', stcl2, sbjcl2)]
+
+school = School('sch1', classes, teachers)
+
+print(f'School classes {school.get_classes()}')
+school.print_class_students('1A')
+school.print_student_subjects('L L.L.')
+school.print_student_parents('L L.L.')
+school.print_class_teachers('1A')
